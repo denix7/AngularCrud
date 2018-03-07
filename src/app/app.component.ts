6aabd1d55822b86cf7d0adbd6c666f1c60efede3
Dennis <dennis.brian.pv@gmail.com>
2018-03-06 21:24:16 -0400

    Agregando tabla para mostrar contenido del componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Angular crud';
 
-  employees: [
-    {'name': 'Dennis', position: 'Programmer'},
-    {'name': 'Brian', position: 'DevOps'},
-    {'name': 'Padilla', position: 'DBadmin'}
+  employees = [
+    {name: 'Dennis', position: 'Programmer', email: 'denis.1594@gmail.com'},
+    {name: 'Brian', position: 'DevOps', email: 'brian.7@gmail.com'},
+    {name: 'Padilla', position: 'DBadmin', email: 'padilla89@gmail.com'}
   ];
 
   //este objeto contendra los datos temporales para luego actualizar el array
   model: any = {};
 
   addEmployee(): void{
-
+    this.employees.push(this.model);
   }
 
   deleteEmployee(): void{
